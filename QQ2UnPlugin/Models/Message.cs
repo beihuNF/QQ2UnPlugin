bf209e0e29ecfaa0732555dd985a2fdbd7a8170e
beihuNF <beihuNF@outlook.com>
2025-02-18 18:44:38 +0800

    First Upload

---
+++ QQ2UnPlugin/Models/Message.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Serialization;
+
+namespace beihuNF.QQ2UnPlugin.Models
+{
+    public sealed class Message
+    {
+        public Message(string text, string iconUrl, string color)
+        {
+            Text = text;
+            IconUrl = iconUrl;
+            Color = color;
+        }
+
+        public Message() { }
+
+        [XmlAttribute]
+        public string Text { get; set; }
+        [XmlAttribute]
+        public string IconUrl { get; set; }
+        public bool ShouldSerializeIconUrl() => !string.IsNullOrEmpty(IconUrl);
+        [XmlAttribute]
+        public string Color { get; set; }
+        public bool ShouldSerializeColor() => !string.IsNullOrEmpty(Color);
+    }
+}
